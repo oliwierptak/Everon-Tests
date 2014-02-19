cff53ae6bcaab588832cb3a2e964faf5dc8f9d32
oliwierptak <oliwierptak@gmail.com>
2014-02-19 11:36:20 +0100

    added Tests subdirectory

---
+++ Everon/Tests/bootstrap_test.php
@@ -0,0 +1,16 @@
+<?php
+namespace Everon;
+
+$CustomExceptionHandler = function(){}; //disable default exception handler
+
+require_once(
+implode(DIRECTORY_SEPARATOR,
+    [dirname(__FILE__), '..', '..', 'Config', 'Bootstrap', 'rest.php'])
+);
+
+//cleanup global state after bootstrap, otherwise phpunit will complain, and $backupGlobalsBlacklist does not work
+unset($CustomExceptionHandler);
+unset($nesting);
+unset($Factory);
+unset($Container);
+unset($Environment);
