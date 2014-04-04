140d41154c35541b3a561501cb0a5c20ff858c8a
oliwierptak <oliwierptak@gmail.com>
2014-04-04 11:11:13 +0200

    added missing test doubles

---
+++ doubles/Rest/Resource/Manager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\Test\Rest\Resource;
+
+
+class Manager extends \Everon\Rest\Resource\Handler implements \Everon\Test\Rest\Interfaces\ResourceManager
+{
+
+}
