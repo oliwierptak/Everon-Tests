20fd90709dd04e24d648f9cdaeaef4348a83557e
oliwierptak <oliwierptak@gmail.com>
2014-05-07 00:24:30 +0200

    - added Cookies to Http Response - updated Tests

--- unit/Http/ResponseTest.php
+++ unit/Http/ResponseTest.php
@@ -14,7 +14,8 @@ class ResponseTest extends \Everon\TestCase
     public function testConstructor()
     {
         $HeadersMock = $this->getMock('Everon\Http\Interfaces\HeaderCollection', [], [], '', false);
-        $Response = new \Everon\Http\Response('RequestIdentifier', $HeadersMock);
+        $CookiesMock = $this->getMock('Everon\Http\Interfaces\CookieCollection', [], [], '', false);
+        $Response = new \Everon\Http\Response('RequestIdentifier', $HeadersMock, $CookiesMock);
         $this->assertInstanceOf('Everon\Http\Interfaces\Response', $Response);
     }
 
@@ -68,6 +69,50 @@ class ResponseTest extends \Everon\TestCase
         $this->assertEquals('test', $text);
         $this->assertEquals('text/plain', $Response->getContentType());
     }
+
+    /**
+     * @dataProvider dataProvider
+     * @runInSeparateProcess
+     */
+    public function testAddCookie(\Everon\Http\Interfaces\Response $Response)
+    {
+        /*$CookieMock = $this->getMock('Everon\Http\Interfaces\Cookie', [], [], '', false);
+        $CookieMock->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue('test'));
+        $CookieMock->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('test it'));
+        $CookieMock->expects($this->once())
+            ->method('getPath')
+            ->will($this->returnValue('/'));
+        $CookieMock->expects($this->once())
+            ->method('getDomain')
+            ->will($this->returnValue('www.example.com'));
+        $CookieMock->expects($this->once())
+            ->method('isSecure')
+            ->will($this->returnValue(false));
+        $CookieMock->expects($this->once())
+            ->method('isHttpOnly')
+            ->will($this->returnValue(false));*/
+        
+        //$Cookie = new \Everon\Http\Cookie('test', 'test it', date('2012-12-01 12:00:00', time()));
+        $Cookie = new \Everon\Http\Cookie('test', 'test it', 0);
+        
+        $Response->addCookie($Cookie);
+
+        $Response->setData('test');
+        $text = $Response->toText();
+        
+        $headers = xdebug_get_headers();
+
+        $this->assertEquals($headers[0], 'Set-Cookie: test=test+it; path=/; httponly');
+        $this->assertEquals($headers[1], 'content-type: text/plain; charset="utf-8"');
+        $this->assertEquals($headers[2], 'EVRID: RequestIdentifier');
+        $this->assertInternalType('string', $text);
+        $this->assertEquals('test', $text);
+        $this->assertEquals('text/plain', $Response->getContentType());
+    }
     
     public function dataProvider()
     {
@@ -76,7 +121,8 @@ class ResponseTest extends \Everon\TestCase
          */
         $Factory = $this->buildFactory();
         $Headers = $Factory->buildHttpHeaderCollection([]); //cant use mock, phpunit complains about file not found
-        $Response = $Factory->buildHttpResponse('RequestIdentifier', $Headers);
+        $Cookies = $Factory->buildHttpCookieCollection([]);
+        $Response = $Factory->buildHttpResponse('RequestIdentifier', $Headers, $Cookies);
 
         return [
             [$Response]
